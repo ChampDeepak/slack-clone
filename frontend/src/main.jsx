8c1582f22f5b6fcd264152d6efa8d6e290aadf9f
ChampDeepak <sharmajijatin27@gmail.com>
2025-10-24 23:59:10 +0530

    Auth Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,11 @@ import {
   createRoutesFromChildren,
   matchRoutes,
 } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from 'react-hot-toast'
+import AuthProvider from './providers/AuthProvide.jsx'
+
+const queryClient = new QueryClient();
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -23,7 +28,12 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
        <BrowserRouter>
-         <App/>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App/>
+          </AuthProvider>
+         <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
