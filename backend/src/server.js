6f2659ad45852cb4e76c596ac4201261fae7a26b
ChampDeepak <sharmajijatin27@gmail.com>
2025-10-25 17:13:01 +0530

    Video Call Feature Implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ import cors from "cors";
 
 // Middleware Calls
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173", credentials: true})); 
+app.use(cors({origin: process.env.origin, credentials: true})); 
 app.use(clerkMiddleware()) //req.auth will be available in the req obj 
 
 
